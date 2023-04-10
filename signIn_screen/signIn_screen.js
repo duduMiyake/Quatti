d8dca8af4446e1840bfad1573e3495a424f3dd11
Eduardo Miyake <90785757+duduMiyake@users.noreply.github.com>
2023-04-10 18:24:48 -0300

    Add files via upload

--- signIn_screen/signIn_screen.js
+++ signIn_screen/signIn_screen.js
@@ -7,7 +7,7 @@ btnAcesso.addEventListener('click', () => {
     let emailInput = document.getElementById('InputEmail').value
     let senhaInput = document.getElementById('InputPassword').value
 
-    if(emailInput==usuario.value && senhaInput==senha.value) {
+    if(emailInput=="teste@email.com" && senhaInput==123456) {
         window.location.href = "https://www.twitch.tv/baiano"
     } else {
         erroMessage.style.visibility = 'visible'
