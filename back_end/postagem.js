ef0b2377dcd4d316940462dce1618a79cf12396e
LeoMarques1206 <111006186+LeoMarques1206@users.noreply.github.com>
2023-06-11 13:36:08 -0300

    Add files via upload

---
+++ back_end/postagem.js
@@ -0,0 +1,81 @@
+const {MongoClient, ObjectId} = require("mongodb");
+async function connect(){
+  if(global.db) return global.db;
+    const conn = await MongoClient.connect("mongodb+srv://quatti:senhadoquatti@cluster0.nyfre0p.mongodb.net/?retryWrites=true&w=majority");
+  if(!conn) return new Error("Can't connect");
+    global.db = await conn.db("quatti");
+  return global.db;
+}
+
+const express = require('express');
+const app = express();         
+const port = 3000; 
+app.use(require('cors')());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+
+
+
+/* GET postagem */
+router.get('/postagem/:id?', async function(req, res, next) {
+    try{
+      const db = await connect();
+      if(req.params.id)
+        res.json(await db.collection("postagem").findOne({_id: new ObjectId(req.params.id)}));
+      else
+        res.json(await db.collection("postagem").find().toArray());
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// POST 
+router.post('/postagem', async function(req, res, next){
+    try{
+      const postagem = req.body;
+      const db = await connect();
+      res.json(await db.collection("postagem").insertOne(postagem));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// PUT 
+router.put('/postagem/:id', async function(req, res, next){
+    try{
+      const postagem = req.body;
+      const db = await connect();
+      res.json(await db.collection("postagem").updateOne({_id: new ObjectId(req.params.id)}, {$set: postagem}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+// DELETE 
+router.delete('/postagem/:id', async function(req, res, next){
+    try{
+      const db = await connect();
+      res.json(await db.collection("postagem").deleteOne({_id: new ObjectId(req.params.id)}));
+    }
+    catch(ex){
+      console.log(ex);
+      res.status(400).json({erro: `${ex}`});
+    }
+})
+
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
